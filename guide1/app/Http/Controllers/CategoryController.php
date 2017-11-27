6aa58f586dee18e023e6d51d01878400507106ea
mohammad ass3d <abdgm87@gmail.com>
2017-11-27 14:28:30 +0200

    slider add

--- guide1/app/Http/Controllers/CategoryController.php
+++ guide1/app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\MarketCategory;
 use Response;
+use App\Category;
 class CategoryController extends Controller
 {
     public function show(){
